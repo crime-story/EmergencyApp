cec772d54443762620d93096689072330279eef5
Voiculescu Alina <78559733+alinavoiculescu@users.noreply.github.com>
2023-03-20 23:43:17 +0200

    small improvements

--- app/src/main/java/com/lifeSavers/emergencyapp/activities/StartupActivity.kt
+++ app/src/main/java/com/lifeSavers/emergencyapp/activities/StartupActivity.kt
@@ -4,17 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.WindowManager
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import com.lifeSavers.emergencyapp.R
 
 class StartupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        val actionBar: ActionBar? = supportActionBar
-        
-        if (actionBar != null) {
-            actionBar.hide()
-        }
+
+        supportActionBar?.hide()
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
